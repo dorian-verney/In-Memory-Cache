82bdb467a5c9351453f835167acb5c2bdb50a3ae
Dorian <verney.dorian@outlook.com>
2026-03-21 12:36:18 +0100

    refactored main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
 import benchmark.PerfThroughput;
 
-void main() throws InterruptedException, IOException {
-//    CacheServer server = new CacheServer();
-//    server.start(6379);
-
-    PerfThroughput perfThroughput = new PerfThroughput();
-
+void main() throws InterruptedException, IOException
+{
+    CacheServer server = new CacheServer();
+    server.start(6379);
 }
